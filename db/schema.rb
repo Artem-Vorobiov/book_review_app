1659c0f34997ad2f85194522126f38caea0b7b80
Artem-Vorobiov <artem-vorobiov_pavlovich@mail.ru>
2017-01-04 22:14:30 +0300

    Add Review model, controller, assosiations, views...

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170102091711) do
+ActiveRecord::Schema.define(version: 20170104094916) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -30,6 +30,15 @@ ActiveRecord::Schema.define(version: 20170102091711) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer  "raiting"
+    t.text     "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "book_id"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
